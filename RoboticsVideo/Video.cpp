df42970386bc4f7e6045b5059cdcd7e510b047e6
Bryson Gullett <brysongullett@gmail.com>
2017-01-21 15:36:25 -0500

    Updated C++ code to be built and deployed on RPi3

--- RoboticsVideo/Video.cpp
+++ RoboticsVideo/Video.cpp
@@ -9,45 +9,49 @@
 #include <netdb.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <netinet/in.h>
 
 using namespace cv;
 using namespace std;
 
-// Values to be posted to Network Tables
-
+// Values to be sent over network
 // See if the target is the boiler, lift, or neither; 1 = boiler 2 = lift 3 = neither
 int targetType = 0;
 
 int frameIndex = 0;
 
-double bigTargetX = 0;
-double bigTargetY = 0;
-double bigTargetWidth = 0;
-double bigTargetHeight = 0;
+int bigTargetX = 0;
+int bigTargetY = 0;
+int bigTargetWidth = 0;
+int bigTargetHeight = 0;
 
-double smallTargetX = 0;
-double smallTargetY = 0;
-double smallTargetWidth = 0;
-double smallTargetHeight = 0;
+int smallTargetX = 0;
+int smallTargetY = 0;
+int smallTargetWidth = 0;
+int smallTargetHeight = 0;
 
-double liftX = 0;
+int liftX = 0;
 
-double bigTargetXDec = 0.0;
-double bigTargetYDec = 0.0;
-double bigTargetHeightDec = 0.0;
+#define DO_TRACES 0
+// Data structures to hold host information and the server information
 
-// Serializes target attributes by broadcasting UDP packets
-void serialize() {
-    
-    // Insert roborio name here
-    const char *host = "roboRIO-3824-FRC.local";
-    
-    // Data structures to hold host information and the server information
-    struct hostent *hp;
-    struct sockaddr_in servaddr;
+const char *host = "roboRIO-3824-FRC.local";    // Insert roborio name here
+struct hostent *hp;
+struct sockaddr_in servaddr;
+int fd;
+
+// Constant value to verify data structures are the same on both this side (client side) and the server side
+int STRUCT_VERSION = 0;
+
+void destructNetwork() {
+    close(fd);
+}
+
+void initNetwork() {
     
     // Creates socket object and see if socket can be created
-    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (fd < 0) {
         perror("cannot create socket");
     }
@@ -60,6 +64,29 @@ void serialize() {
     // Look up the address of the server given its name
     hp = gethostbyname(host);
     if (!hp) {
+        destructNetwork();
+        fprintf(stderr, "could not obtain address of %s\n", host);
+    }
+    
+    // Put the host's address into the server address structure
+    memcpy((void *)&servaddr.sin_addr, hp->h_addr_list[0], hp->h_length);
+}
+
+void dump_buf(unsigned char *buf, int len)
+{
+    unsigned char *b = buf;
+    for (int i=0; i<len; i++)
+    {
+        printf("%02X ", *b++);
+    }
+    
+    printf("\n");
+}
+
+// Serializes target attributes by broadcasting UDP packets
+void serialize() {
+    
+    if (!hp) {
         fprintf(stderr, "could not obtain address of %s\n", host);
     } else {
         
@@ -67,24 +94,19 @@ void serialize() {
             bigTargetX = liftX;
         }
         
-        bigTargetXDec = (bigTargetX - (int)bigTargetX) * 10;
-        bigTargetYDec = (bigTargetY - (int)bigTargetY) * 10;
-        bigTargetHeightDec = (bigTargetHeight - (int)bigTargetHeight) * 10;
-        
         // Data structure that contains the values to be sent to server (roborio) via UDP packet
-        struct targetInformation {
-            short frameNum = htons(frameIndex);
-            short targetXInt = htons((short)bigTargetX);
-            short targetYInt = htons((short)bigTargetY);
-            short targetHeightInt = htons((short)bigTargetHeight);
-            short targetXDecimal = htons((short)bigTargetXDec);
-            short targetYDecimal = htons((short)bigTargetYDec);
-            short targetHeightDecimal = htons((short)bigTargetHeightDec);
-            char targetNum = (char)targetType;
+        struct __attribute__((packed)) targetInformation {
+            unsigned char  dataStructVersion = (unsigned char) STRUCT_VERSION;
+            unsigned char  targetNum = (unsigned char) targetType;
+            unsigned short frameNum = htons((unsigned short) frameIndex);
+            unsigned short targetX = htons((unsigned short)bigTargetX);
+            unsigned short targetY = htons((unsigned short)bigTargetY);
+            unsigned short targetHeight = htons((short)bigTargetHeight);
         } values;
         
-        // Put the host's address into the server address structure
-        memcpy((void *)&servaddr.sin_addr, hp->h_addr_list[0], hp->h_length);
+#if (DO_TRACES == 1)
+        dump_buf((unsigned char *) &values, sizeof(values));
+#endif
         
         // Sends the frame number, target type (lift or boiler), target x coordinate, target y coordinate, and target height to the server (roborio)
         if (sendto(fd, &values, sizeof(values), 0, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
@@ -104,12 +126,14 @@ int main() {
         return 1;
     }
     
+    initNetwork();
+    
     // Raw image from camera
     Mat cameraFrameImage;
     
     // Default scalar constants for program to only detect reflective tape (H, S, V)
-    //    Scalar MINCOLOR = Scalar(49, 128, 76);
-    //    Scalar MAXCOLOR = Scalar(78, 255, 255);
+//    Scalar MINCOLOR = Scalar(49, 128, 76);
+//    Scalar MAXCOLOR = Scalar(78, 255, 255);
     
     // Test scalar constants for program to detect more than just the reflective tape (H, S, V)
     Scalar MINCOLOR = Scalar(20, 20, 20);
@@ -121,24 +145,29 @@ int main() {
     // Image to display in window
     Mat outputImg;
     
-    // Vector holding contours
-    vector<vector<Point> > contours;
+    // Vectors holding contour and contour attributes
+    vector<vector<Point> > inputContours;
+    vector<vector<Point> > outputContours;
     vector<Vec4i> hierarchy;
+    vector<Point> hull;
     
-    // Variables used to identify largest contours
-    int largestArea = 0;
-    int secondLargestArea = 0;
-    int largestContourIndex = 0;
-    int secondLargestContourIndex = 0;
-    
-    // Rectangles that will be wrapped around the pieces of reflective tape
-    Rect largestTapeRect;
-    Rect secondLargestTapeRect;
+    int MIN_CONTOUR_WIDTH = 100;
+    int MAX_CONTOUR_WIDTH = 1000;
+    int MIN_CONTOUR_HEIGHT = 100;
+    int MAX_CONTOUR_HEIGHT = 1000;
+    int MIN_CONTOUR_AREA = 100;
+    int MIN_CONTOUR_PERIMETER = 100;
+    int MIN_SOLIDITY = 0;
+    int MAX_SOLIDITY = 100;
+    int MIN_VERTEX_COUNT = 0;
+    int MAX_VERTEX_COUNT = 1000000;
+    int MIN_RATIO = 0;
+    int MAX_RATIO = 1000;
     
     // Write only file storage system for outputs.xml file, where matrices containing images will be stored
-    FileStorage fileStorage("outputs.xml", FileStorage::WRITE);
+//    FileStorage fileStorage("outputs.xml", FileStorage::WRITE);
     
-    int frameStorageIndex = 0;
+//    int frameStorageIndex = 0;
     
     // Displays video feed and writes every tenth frame to outputs.xml
     while (true) {
@@ -157,21 +186,59 @@ int main() {
         // Makes the contour image only show elements that are in between the specified HSV min and max constants
         inRange(cameraFrameImage, MINCOLOR, MAXCOLOR, contourImg);
         
-        
         //Finds the contours on the contour image
-        findContours(contourImg, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+        findContours(contourImg, inputContours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+        
+        outputContours.clear();
+        
+        for (vector<Point> contour: inputContours) {
+            Rect bb = boundingRect(contour);
+            if (bb.width > MIN_CONTOUR_WIDTH && bb.width < MAX_CONTOUR_WIDTH) {
+                if (bb.height > MIN_CONTOUR_HEIGHT && bb.height < MAX_CONTOUR_HEIGHT) {
+                    double area = contourArea(contour);
+                    if (area > MIN_CONTOUR_AREA) {
+                        if (arcLength(contour, true) > MIN_CONTOUR_PERIMETER) {
+                            convexHull(Mat(contour, true), hull);
+                            double solid = 100 * area / contourArea(hull);
+                            if (solid > MIN_SOLIDITY && solid < MAX_SOLIDITY) {
+                                if (contour.size() > MIN_VERTEX_COUNT && contour.size() < MAX_VERTEX_COUNT)	{
+                                double ratio = bb.width / bb.height;
+                                    if (ratio > MIN_RATIO && ratio < MAX_RATIO) {
+                                        outputContours.push_back(contour);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        for (int outputContourIndex = 0; outputContourIndex < outputContours.size(); outputContourIndex++) {
+            drawContours(contourImg, outputContours, outputContourIndex, CV_RGB(255, 255, 255), 2, 8, hierarchy, 0, Point() );
+        }
+        
+        // Variables used to identify largest contours
+        int largestArea = 0;
+        int secondLargestArea = 0;
+        int largestContourIndex = 0;
+        int secondLargestContourIndex = 0;
+        
+        // Rectangles that will be wrapped around the pieces of reflective tape
+        Rect largestTapeRect;
+        Rect secondLargestTapeRect;
         
         // Finds largest contour; makes largestTapeRect only wrap around the largest contour (target)
-        for (int contourIndex = 0; contourIndex < contours.size(); contourIndex++) {
-            double a = contourArea(contours[contourIndex], false);
+        for (int contourIndex = 0; contourIndex < outputContours.size(); contourIndex++) {
+            double a = contourArea(outputContours[contourIndex], false);
             if (a > largestArea){
                 largestArea = a;
                 largestContourIndex = contourIndex;
-                largestTapeRect = boundingRect( Mat(contours[contourIndex]) );
+                largestTapeRect = boundingRect( Mat(outputContours[contourIndex]) );
             } else if (a < largestArea && a > secondLargestArea) {
                 secondLargestArea = a;
                 secondLargestContourIndex = contourIndex;
-                secondLargestTapeRect = boundingRect( Mat(contours[contourIndex]) );
+                secondLargestTapeRect = boundingRect( Mat(outputContours[contourIndex]) );
             }
         }
         
@@ -193,7 +260,12 @@ int main() {
         // Dectect to see if the strips of reflective tape are for the boiler or the lift
         double expectedBoilerProportion = 2.0;
         double expectedLiftProportion = 1.0;
-        double actualProportion = bigTargetHeight/smallTargetHeight;
+        double actualProportion;
+        
+        if (smallTargetX != 0) {
+            actualProportion = bigTargetHeight/smallTargetHeight;
+        }
+        
         double deviationFromExpectedBoilerProportion = expectedBoilerProportion - actualProportion;
         double deviationFromExpectedLiftProportion = expectedLiftProportion - actualProportion;
         
@@ -208,24 +280,26 @@ int main() {
         imshow("outputImage", outputImg);
         
         // Writes every tenth frame that the window is showing to outputs.xml file
-        if (frameIndex % 10 == 0) {
-            stringstream name;
-            name << "outputImage" << frameStorageIndex;
-            fileStorage << name.str() << outputImg;
-            frameStorageIndex++;
-        }
+//        if (frameIndex % 10 == 0) {
+//            stringstream name;
+//            name << "outputImage" << frameStorageIndex;
+//            fileStorage << name.str() << outputImg;
+//            frameStorageIndex++;
+//        }
         
         serialize();
         
         frameIndex++;
         
         // Breaks the loop to end the program if the next image does not exist
-        if (waitKey(30) >= 0)
+        if (((char) waitKey(30)) >= 0)
+        {
             break;
+        }
     }
     
     // Releases (ends) the file storage system
-    fileStorage.release();
+//    fileStorage.release();
     
     return 0;
 }
